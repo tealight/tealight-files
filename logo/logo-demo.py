c9007dc0b64732ed7fb6ce3bad9c826b2e90125b
Ian Davies <git@iandavies.org>
2013-08-25 13:01:59 +0100

    Initial sample code.

---
+++ logo/logo-demo.py
@@ -0,0 +1,7 @@
+from tealight.logo import move, turn, 
+                          color, 
+                          pen_up, pen_down, 
+                          show_turtle, hide_turtle
+
+
+print "This is logo mode!"
---
+++ robot/robot-demo.py
@@ -0,0 +1,2 @@
+
+print "This is robot mode!"
