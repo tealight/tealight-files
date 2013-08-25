9d3300def67a3126dc33b9b1d00a7f7ae07bf0b1
Ian Davies <git@iandavies.org>
2013-08-25 23:06:39 +0100

    Update logo demo.

--- logo/logo-demo.py
+++ logo/logo-demo.py
@@ -1,7 +1,15 @@
-from tealight.logo import move, turn, 
-                          color, 
-                          pen_up, pen_down, 
-                          show_turtle, hide_turtle
+from logo import (move, turn,
+                  pen_down, pen_up,
+                  show_turtle, hide_turtle,
+                  color, speed)
 
 
 print "This is logo mode!"
+
+colors = ["red", "blue", "green"]
+
+for i in range(10,200,5):
+  move(i)
+  turn(80)
+  c = colors[(i / 5)%3]
+  color(c)
